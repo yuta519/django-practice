61ee576f153015d310bd63494678ba632e3734e7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 15:43:46 -0700

    Creates new tasks app

--- project/polls/tests.py
+++ project/polls/tests.py
@@ -32,7 +32,7 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        self.assertIs(recent_question.was_published_recently(), False)
 
 
 def create_question(question_text, days):

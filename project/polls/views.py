4d3886651794398d1989c762c77ddb93725ad003
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 08:06:57 -0700

    Adds more views

--- project/polls/views.py
+++ project/polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")

ffe89bfc2da2f946362ffdb8a2590c62318271d1
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 08:10:14 -0700

    Adds url paths to urls.py

--- project/polls/urls.py
+++ project/polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path("", views.index, name="polls index")
+    path("", views.index, name="polls index"),
+    path("<int:question_id>/", views.detail, name="polls detail"),
+    path("<int:question_id>/results/", views.results, name="polls results"),
+    path("<int:question_id>/vote/", views.vote, name="polls vote"),
 ]

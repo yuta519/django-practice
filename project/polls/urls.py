1edf13ad686e947675657954733a2908a324144d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 10:48:19 -0700

    Uses namespace

--- project/polls/urls.py
+++ project/polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from polls import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="polls index"),
-    path("<int:question_id>/", views.detail, name="polls detail"),
-    path("<int:question_id>/results/", views.results, name="polls results"),
-    path("<int:question_id>/vote/", views.vote, name="polls vote"),
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]

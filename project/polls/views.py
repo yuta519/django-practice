40f877d5abcdb0a40e1c131a7069c2ea194c715a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 10:21:53 -0700

    Changes polls index to show all results

--- project/polls/views.py
+++ project/polls/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list =  Question.objects.order_by("-pub_date")[:5]
+    return HttpResponse(", ".join([q.question_text for q in latest_question_list]))
 
 def detail(request, question_id):
     return HttpResponse(f"You are looking at question {question_id}")

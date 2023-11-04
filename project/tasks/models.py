b4799163a61bcca92b01688b04e3e504264cf05a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 16:48:50 -0700

    Creates initial models for tasks

--- project/tasks/models.py
+++ project/tasks/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.forms import ValidationError
 
-# Create your models here.
+
+class Status(models.IntegerChoices):
+    TODO = 1, 'ToDo'
+    ONGOING = 2, 'Ongoing'
+    PENDING = 3, "Pending"
+    REVIEW = 4, "Review"
+    DONE = 5, "Done"
+
+    def __str__(self):
+        return [i.value for i in Status]
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=200)
+    status = models.IntegerField(choices=Status.choices, default=Status.TODO)
+    assigned_to = models.ForeignKey(
+        User, related_name="user", on_delete=models.CASCADE, null=True, default=None
+    )
+    created_at = models.DateField("Created Time",  auto_now_add=True)
+    updated_at = models.DateField("Updated Time",  auto_now=True)
+
+    def clean(self):
+       if self.status != Status.TODO and not self.assigned_to:
+        raise ValidationError("You must set assigner unless status is ToDo.")
+
+    def __str__(self):
+        return f'{self.title} by {self.assigned_to.name}'

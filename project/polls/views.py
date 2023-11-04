4610b0690200312405064a0e9bd538629444b594
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 11:22:39 -0700

    Replaces functional view to class-based view

--- project/polls/views.py
+++ project/polls/views.py
@@ -1,29 +1,29 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-# from django.template import loader
+from django.views import generic
 
-from polls.models import Choice, Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = { "latest_question_list": latest_question_list }
-    # template = loader.get_template("polls/index.html")
-    # return HttpResponse(template.render(context, request))
-    return render(request, "polls/index.html", context)
-
-def detail(request, question_id):
-    try:
-        q = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("Question does not exist.")
-    return render(request, "polls/detail.html", {"question": q})
+from .models import Choice, Question
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)

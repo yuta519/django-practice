19b618366b311cf383d8ecad5bc95575b7e8ab6e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-04 10:27:48 -0700

    Creates polls template and render it

--- project/polls/views.py
+++ project/polls/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+# from django.template import loader
 
 from polls.models import Question
 
 
 def index(request):
-    latest_question_list =  Question.objects.order_by("-pub_date")[:5]
-    return HttpResponse(", ".join([q.question_text for q in latest_question_list]))
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = { "latest_question_list": latest_question_list }
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse(f"You are looking at question {question_id}")

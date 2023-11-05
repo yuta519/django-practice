f0cfab8ea15ed0c7ff798a4d73df49dd83646c5d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-05 11:45:33 -0800

    Creates url path to update ticket and assign ticket to user

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("users/<int:pk>", views.UserDetailView.as_view(), name="user_detail"),
     path("tickets/", views.TicketIndexView.as_view(), name="tickets"),
     path("tickets/<int:pk>", views.TicketDetailView.as_view(), name="ticket_detail"),
+    path("tickets/<int:pk>/update", views.TicketUpdateView.as_view(), name="ticket_update"),
+    path("tickets/<int:pk>/assign", views.TicketAssignView.as_view(), name="assign"),
 ]

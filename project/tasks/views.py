458c42ab4496e5a52ad84001b1b95e9fec69d6f9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-05 11:46:36 -0800

    Used generic.updateview

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.views import generic
 
 from tasks.models import Ticket, User
@@ -21,3 +22,26 @@ class TicketIndexView(generic.ListView):
 class TicketDetailView(generic.DetailView):
     model = Ticket
     template_name ="tasks/ticket_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(TicketDetailView, self).get_context_data(**kwargs)
+        context["users"] = User.objects.all()
+        return context
+
+
+class TicketUpdateView(generic.UpdateView):
+    model = Ticket
+    template_name ="tasks/ticket_update.html"
+    fields = ['title', 'status', 'assigned_to']
+    # success_url = '/tasks/tickets'
+
+    def get_success_url(self) -> str:
+        return reverse('tasks:tickets')
+
+class TicketAssignView(generic.UpdateView):
+    model = Ticket
+    template_name ="tasks/ticket_assign.html"
+    fields = ['assigned_to']
+
+    def get_success_url(self) -> str:
+        return reverse('tasks:tickets')

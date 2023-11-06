afdf309f71d2d5330ad228ec8883927c26ad7726
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-06 10:36:30 -0800

    Adds perform_create

--- drf/drf/snipets/urls.py
+++ drf/drf/snipets/urls.py
@@ -12,5 +12,7 @@ router.register(r'snippets/<int:pk>', views.SnippetDetail.as_view(), basename='s
 
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view())
 ]

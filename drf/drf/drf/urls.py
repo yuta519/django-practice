43b7efc3861d9a5427ebf3503217042b5f500f57
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-06 09:40:20 -0800

    Creates new urls for snippets

--- drf/drf/drf/urls.py
+++ drf/drf/drf/urls.py
@@ -10,5 +10,6 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/', include('snipets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
---
+++ drf/drf/snipets/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from snipets import views
+
+
+router = routers.DefaultRouter()
+router.register(r'snippets', views.SnippetList.as_view(), basename='snippet_list')
+router.register(r'snippets/<int:pk>', views.SnippetDetail.as_view(), basename='snippet_detail')
+
+
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
+]
